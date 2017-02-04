df12c684ba77819fc99cf8e6668318c1e35a1a6c
Woods2016 <ljz00001@163.com>
2017-02-04 12:42:23 +0800

    aaa

--- src/com/zhou/Main.java
+++ src/com/zhou/Main.java
@@ -1,9 +1,13 @@
 package com.zhou;
 
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("不同用户测试");
         System.out.println("并发编程测试");
+        List<String> list = null;
+        System.out.println(list.isEmpty());
     }
 }
