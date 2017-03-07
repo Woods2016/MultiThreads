54a0b52240161d713bd02a3ae108139f65884ae0
Woods2016 <ljz00001@163.com>
2017-03-07 16:49:13 +0800

    修改测试

--- src/com/zhou/Main.java
+++ src/com/zhou/Main.java
@@ -6,7 +6,8 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("test");
+        System.out.println("test111");
+        System.out.println("branch2");
         List<String> list = new ArrayList<>();
         System.out.println(list.isEmpty());
     }
