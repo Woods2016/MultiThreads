9b7b9f4b25c45ac007e330258f3f9211e6671a36
Woods2016 <ljz00001@163.com>
2017-03-07 19:44:32 +0800

    Future/FutureTask

---
+++ src/main/java/com/future/TestFutureTask.java
@@ -0,0 +1,55 @@
+package com.future;
+
+import org.junit.Test;
+
+import java.util.concurrent.*;
+
+/**
+ * @Description: 简单测试FutureTask
+ * @Author: ZhOu
+ * @Date: 2017/3/7
+ */
+public class TestFutureTask {
+
+    //使用Thread方式执行
+    @Test
+    public void test1() {
+        Callable<String> callable = getStringCallable();
+
+        FutureTask<String> futureTask = new FutureTask<>(callable);
+        Thread thread = new Thread(futureTask);
+        thread.start();
+
+        try {
+            System.out.println("执行结果：" + futureTask.get());
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //使用ExecutorServer执行
+    @Test
+    public void test2() {
+        Callable<String> callable = getStringCallable();
+        FutureTask<String> futureTask = new FutureTask<>(callable);
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        executorService.submit(futureTask);
+        try {
+            System.out.println("执行结果：" + futureTask.get());
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+        executorService.shutdown();
+    }
+
+    private Callable<String> getStringCallable() {
+        return () -> {
+            System.out.println("start time=" + System.currentTimeMillis());
+            System.out.println("sleep start");
+            Thread.sleep(5000);
+            System.out.println("sleep end");
+            return "end time=" + System.currentTimeMillis();
+        };
+    }
+
+}
