482db927ab691a397d5288da91f99791369e445b
Woods2016 <ljz00001@163.com>
2017-03-07 16:49:13 +0800

    修改测试

--- src/com/zhou/Main.java
+++ src/com/zhou/Main.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("test");
+        System.out.println("test111");
         List<String> list = new ArrayList<>();
         System.out.println(list.isEmpty());
     }
