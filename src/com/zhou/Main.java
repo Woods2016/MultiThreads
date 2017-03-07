607720accc121635a755c8b24a9b22ca136c1c3c
Woods2016 <ljz00001@163.com>
2017-03-07 16:41:48 +0800

    修改测试

--- src/com/zhou/Main.java
+++ src/com/zhou/Main.java
@@ -1,13 +1,13 @@
 package com.zhou;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("不同用户测试");
-        System.out.println("并发编程测试");
-        List<String> list = null;
+        System.out.println("test");
+        List<String> list = new ArrayList<>();
         System.out.println(list.isEmpty());
     }
 }
