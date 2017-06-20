99299ed09f7318b475b839342aefa28c98d8638f
Woods2016 <ljz00001@163.com>
2017-06-20 21:58:15 +0800

    守护线程

---
+++ src/main/java/com/daemon/DaemonDemo.java
@@ -0,0 +1,32 @@
+package com.daemon;
+
+/**
+ * @Description: 守护线程
+ * @Author: ZhOu
+ * @Date: 2017/6/20
+ */
+public class DaemonDemo {
+
+
+    public static void main(String[] args) throws InterruptedException {
+        DaemonDemo.ThreadDaemon t = new DaemonDemo.ThreadDaemon();
+        t.setDaemon(true);
+        t.start();
+        Thread.sleep(2000L);
+    }
+
+    static class ThreadDaemon extends Thread {
+        @Override
+        public void run() {
+            while (true) {
+                System.out.println("线程执行");
+
+                try {
+                    Thread.sleep(1000L);
+                } catch (InterruptedException var2) {
+                    var2.printStackTrace();
+                }
+            }
+        }
+    }
+}
